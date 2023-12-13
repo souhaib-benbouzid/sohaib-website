15af90250ab6eac10f15393c5cafa220ff2391ce
Souhaib <souhaib.benbouzid@gmail.com>
2023-12-13 22:47:07 +0100

    add daisy ui

--- portfolio/frontend/tailwind.config.ts
+++ portfolio/frontend/tailwind.config.ts
@@ -6,18 +6,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-open_sans'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  theme: {},
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark'],
   },
-  plugins: [],
 };
 export default config;
