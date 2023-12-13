a1c79a79180b1d9d38af488c3f9e34f5e473a3e8
Souhaib <souhaib.benbouzid@gmail.com>
2023-12-13 22:47:26 +0100

    add logo

---
+++ portfolio/frontend/src/components/Logo.tsx
@@ -0,0 +1,153 @@
+interface Props extends React.SVGAttributes<SVGElement> {}
+
+function Logo(props: Props) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 48 48'
+      {...props}
+    >
+      <g clipPath='url(#clip0_16_2363)'>
+        <path
+          fill='url(#paint0_linear_16_2363)'
+          d='M21.396 15.57c.304-.4.523-.59.923-.877.475-.342.781-.49 1.333-.658.543-.165.87-.171 1.435-.192a9.67 9.67 0 011.41.082c.722.094 1.12.208 1.82.411.755.22 1.898.685 1.898.685l-2.641 4.41s-1.059-.51-1.769-.739c-.889-.288-1.405-.439-2.333-.493-.36-.021-.615 0-.794 0-.129 0-.494.022-.667.164-.136.113-.223.203-.256.384-.047.253.256.602.256.602s-.596-.5-.846-.93c-.266-.461-.43-.775-.41-1.316.013-.332.078-.518.205-.822.127-.303.24-.454.436-.712z'
+        ></path>
+        <path
+          fill='url(#paint1_linear_16_2363)'
+          d='M17.287 20.228c.027-1.068.258-2.15.53-2.846a7.225 7.225 0 01.855-1.623c.416-.621.688-.939 1.205-1.398.483-.43 1.396-.927 1.396-.927s-.627.861-.85 1.485c-.217.603-.324.98-.347 1.633-.015.407 0 .642.075 1.028.072.366.138.564.283.88.187.408.346.588.613.9.434.507 1.272 1.005 1.272 1.005l2.715 1.246s1.114.5 1.782.946c.505.338 1.247.972 1.247.972s.719.722 1.136 1.321c.466.67.914 1.805 1.06 2.323.164.576.196 1.332.183 1.848-.013.516-.039 1.515-.501 2.706-.49 1.262-.708 1.513-1.318 2.29-.727.927-2.144 1.865-2.144 1.865s.615-1.198.834-1.801c.04-.11.381-1.008.362-2.245-.015-.93-.766-1.89-2.02-2.586-.426-.236-3.416-1.684-3.717-1.85-.301-.167-1.878-.85-2.8-1.93-.69-.81-1.192-1.688-1.463-2.475-.339-.984-.412-1.803-.388-2.767z'
+        ></path>
+        <path
+          fill='url(#paint2_linear_16_2363)'
+          d='M35.123 43.551c.492 2.451-.967 4.037-.967 4.037s-2.25-1.229-2.742-3.68c-.492-2.45.723-3.777.723-3.777s2.152.918 2.986 3.42z'
+        ></path>
+        <path
+          fill='url(#paint3_linear_16_2363)'
+          d='M41.382 25.096l3.808 4.183-1.458 1.688-9.936 10.135s-1.116 1.182-1.373 2.168c-.297 1.14.283 3.058.283 3.058s-1.202-1.055-1.817-1.872c-.507-.674-.831-1.059-1.116-1.854-.215-.602-.325-.97-.313-1.602.01-.51.09-.795.25-1.27.225-.665.721-1.413 1.038-1.73 2.916-2.92 5.452-5.601 8.984-9.15.996-1 1.288-1.242 1.65-1.653.535-.606 0-2.1 0-2.1z'
+        ></path>
+        <path
+          fill='url(#paint4_linear_16_2363)'
+          d='M37.528 18.907c.191-.948 1.199-2.133 1.199-2.133s5.149 5.045 6.29 6.67c.696.991.889 2.149.898 2.98.01.832.018 1.866-.71 2.874-.633.875-2.756 2.975-2.756 2.975s.933-2.45.628-3.936c-.282-1.372-.92-2-1.728-3.139-1.098-1.545-2.622-2.493-3.38-3.453s-.66-1.749-.44-2.838z'
+        ></path>
+        <path
+          fill='url(#paint5_linear_16_2363)'
+          d='M12.997 4.72c-.487-2.6.957-4.283.957-4.283s2.227 1.303 2.713 3.903c.487 2.6-.715 4.008-.715 4.008s-2.13-.974-2.955-3.629z'
+        ></path>
+        <path
+          fill='url(#paint6_linear_16_2363)'
+          d='M6.803 24.298l-3.767-4.437 1.441-1.791L14.31 7.318s1.104-1.254 1.359-2.3c.294-1.21-.28-3.245-.28-3.245s1.19 1.12 1.798 1.986c.502.716.823 1.124 1.104 1.967.213.64.322 1.03.31 1.7-.01.541-.089.843-.248 1.347-.222.705-.713 1.5-1.026 1.836-2.886 3.097-5.396 5.942-8.89 9.706-.986 1.062-1.275 1.319-1.634 1.755-.529.643 0 2.228 0 2.228z'
+        ></path>
+        <path
+          fill='url(#paint7_linear_16_2363)'
+          d='M10.617 30.865c-.19 1.006-1.186 2.263-1.186 2.263s-5.095-5.352-6.225-7.076c-.688-1.052-.88-2.28-.889-3.162-.01-.883-.017-1.98.703-3.048.627-.929 2.728-3.157 2.728-3.157s-.924 2.6-.622 4.176c.28 1.455.91 2.122 1.71 3.33 1.086 1.64 2.595 2.645 3.345 3.664.75 1.018.653 1.855.436 3.01z'
+        ></path>
+      </g>
+      <defs>
+        <linearGradient
+          id='paint0_linear_16_2363'
+          x1='20.786'
+          x2='28.43'
+          y1='17.861'
+          y2='17.861'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#172645'></stop>
+          <stop offset='1' stopColor='#4FCCF9'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint1_linear_16_2363'
+          x1='18.317'
+          x2='33.534'
+          y1='12.288'
+          y2='29.919'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#FAFAF8'></stop>
+          <stop offset='0.497' stopColor='#6EC6EC'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint2_linear_16_2363'
+          x1='32.759'
+          x2='35.035'
+          y1='42.237'
+          y2='47.309'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#10112B'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint3_linear_16_2363'
+          x1='44.315'
+          x2='27.733'
+          y1='28.081'
+          y2='43.11'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#10112B'></stop>
+          <stop offset='0.565' stopColor='#34B1E7'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint4_linear_16_2363'
+          x1='37.428'
+          x2='45.649'
+          y1='17.504'
+          y2='29.133'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#FAFAF8'></stop>
+          <stop offset='0' stopColor='#FAFAF8'></stop>
+          <stop offset='0' stopColor='#A8DCF1'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint5_linear_16_2363'
+          x1='15.337'
+          x2='12.811'
+          y1='6.114'
+          y2='0.864'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#10112B'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint6_linear_16_2363'
+          x1='3.901'
+          x2='21.331'
+          y1='21.131'
+          y2='6.395'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#10112B'></stop>
+          <stop offset='0.565' stopColor='#34B1E7'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <linearGradient
+          id='paint7_linear_16_2363'
+          x1='10.717'
+          x2='1.809'
+          y1='32.353'
+          y2='20.601'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#FAFAF8'></stop>
+          <stop offset='0' stopColor='#FAFAF8'></stop>
+          <stop offset='0' stopColor='#A8DCF1'></stop>
+          <stop offset='1' stopColor='#34B1E7'></stop>
+        </linearGradient>
+        <clipPath id='clip0_16_2363'>
+          <path
+            fill='#fff'
+            d='M0 0H48.065V48H0z'
+            transform='translate(.128)'
+          ></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default Logo;
