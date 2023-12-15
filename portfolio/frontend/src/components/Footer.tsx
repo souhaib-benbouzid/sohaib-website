42ac304a7f8ab657e73be2a0b841b59aa061cd42
Souhaib <souhaib.benbouzid@gmail.com>
2023-12-15 16:41:07 +0100

    footer

---
+++ portfolio/frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Socials from './Socials';
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className='w-full'>
+      <Socials />
+    </footer>
+  );
+};
+
+export default Footer;
